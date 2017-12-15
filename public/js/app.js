81a7a05617303c533ff1d38c963a8bfae455526b
Ethan Jobmann <ethanlouis_fj@yahoo.com>
2017-12-14 19:34:45 -0500

    removed starter gif array

--- public/js/app.js
+++ public/js/app.js
@@ -5,23 +5,7 @@ app.controller('MainController', ['$http', function($http) {
 
   this.newGifData = {};
 
-  this.gifs = [
-    { title: 'Lando',
-    url: 'https://media.giphy.com/media/3ornk7TgUdhjhTYgta/giphy.gif'},
-
-    { title: 'Darth Vader',
-    url: 'https://media.giphy.com/media/ZX6NHzA9Fcddm/giphy.gif'},
-
-    { title: 'Trap',
-    url: 'https://media.giphy.com/media/8McNH1aXZnVyE/giphy.gif'},
-
-    { title: 'BB-8',
-    url: 'https://media.giphy.com/media/5xaOcLOqNmWHaLeB14I/giphy.gif'},
-
-    { title: 'Yoda',
-    url: 'https://media.giphy.com/media/yDYAHbqe5DfyM/giphy.gif'}
-
-  ];
+  this.gifs = [];
 
   this.newGif = () => {
     $http({
